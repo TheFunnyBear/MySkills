53ada3997619953b720cbc54deb6eb0975e84b84
Denis Kosolapov <denisk@webconsultants.ru>
2018-07-12 20:08:28 +0300

    First attempt to add multi languige support

--- MySkills/Controllers/HomeController.cs
+++ MySkills/Controllers/HomeController.cs
@@ -5,9 +5,44 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text;
+using PdfSharp;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
 
 namespace MySkills.Controllers
 {
+    public static class ControllerExtensions
+    {
+        public static string RenderView(this Controller controller, string viewName, object model)
+        {
+            return RenderView(controller, viewName, new ViewDataDictionary(model));
+        }
+
+        public static string RenderView(this Controller controller, string viewName, ViewDataDictionary viewData)
+        {
+            var controllerContext = controller.ControllerContext;
+
+            var viewResult = ViewEngines.Engines.FindView(controllerContext, viewName, null);
+
+            StringWriter stringWriter;
+
+            using (stringWriter = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    controllerContext,
+                    viewResult.View,
+                    viewData,
+                    controllerContext.Controller.TempData,
+                    stringWriter);
+
+                viewResult.View.Render(viewContext, stringWriter);
+                viewResult.ViewEngine.ReleaseView(controllerContext, viewResult.View);
+            }
+
+            return stringWriter.ToString();
+        }
+    }
+
     public class HomeController : Controller
     {
         // GET: Home
@@ -67,6 +102,61 @@ namespace MySkills.Controllers
             var fileStreamResult = new FileStreamResult(vCardStream, "application/vcf")
             {
                 FileDownloadName = "DenisKosolapovCard.vcf"
+            };
+            return fileStreamResult;
+        }
+
+        private Byte[] PdfSharpConvert(String html)
+        {
+            var config = new PdfGenerateConfig()
+            {
+                MarginBottom = 20,
+                MarginLeft = 40,
+                MarginRight = 20,
+                MarginTop = 20,
+                PageSize = PageSize.A4,
+                PageOrientation = PageOrientation.Portrait
+            };
+            
+            Byte[] res = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var pdf = PdfGenerator.GeneratePdf(html, config);
+                pdf.Save(ms);
+                res = ms.ToArray();
+            }
+            return res;
+        }
+
+        [HttpGet]
+        public FileStreamResult ResumePdfFile()
+        {
+            var htmlView = new StringBuilder();
+            
+            var contacts = new Contacts();
+            htmlView.AppendLine(this.RenderView("ContactsPdfView", contacts));
+
+            var targetDescription = new TargetDescription();
+            htmlView.AppendLine(this.RenderView("TargetDescriptionView", targetDescription));
+
+            var education = new Education();
+            htmlView.AppendLine(this.RenderView("EducationPdfView", education));
+            
+            var stages = new Stages();
+            htmlView.AppendLine(this.RenderView("StagesPdfView", stages));
+
+            var skillsAndAbilities = new SkillsAndAbilities();
+            htmlView.AppendLine(this.RenderView("SkillsAndAbilitiesPdfView", skillsAndAbilities));
+
+            var aboutMe = new AboutMe();
+            htmlView.AppendLine(this.RenderView("AboutMePdfView", aboutMe));
+
+            var pdfArray= PdfSharpConvert(htmlView.ToString());
+
+            var pdfStream = new MemoryStream(pdfArray);
+            var fileStreamResult = new FileStreamResult(pdfStream, "application/pdf")
+            {
+                FileDownloadName = "DenisKosolapov.pdf"
             };
             return fileStreamResult;
         }
--- MySkills/Global.asax.cs
+++ MySkills/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -14,5 +16,17 @@ namespace MySkills
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            string culture = "en-US";
+            if (Request.UserLanguages != null)
+            {
+                culture = Request.UserLanguages[0];
+            }
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(culture);
+        }
+
     }
 }
